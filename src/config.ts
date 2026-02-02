b1a334cbab127d88cd1a7f80042166c0aeb9f878
Alfonso Reyes <alfonso@molecula.digital>
2026-02-02 08:44:03 -0700

    refactor: create components for common items, move links into config file

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,67 @@
 
 export const SITE_TITLE = "alfon.so";
 export const SITE_DESCRIPTION = "Bienvenido a mi terrenito en internet";
+
+export interface NavLink {
+  title: string;
+  id?: string;
+  url?: string;
+  redirect?: string;
+  tooltip?: string;
+}
+
+export const NAV_LINKS: NavLink[] = [
+  {
+    title: "index.html",
+    id: "home",
+    url: "/",
+    tooltip: "Inicio",
+  },
+  {
+    title: "blog.sql",
+    id: "blog",
+    url: "/blog",
+    tooltip: "Mis posts",
+  },
+  {
+    title: "photos.jpg",
+    redirect: "https://ojoanalogo.com",
+    tooltip: "Mi vida en fotos",
+  },
+  {
+    title: "startup.sh",
+    redirect: "https://molecula.digital",
+    tooltip: "Mi startup de productos digitales",
+  },
+];
+
+export type SocialPlatform = "twitter" | "github" | "instagram" | "linkedin";
+
+export interface SocialLink {
+  platform: SocialPlatform;
+  url: string;
+  label: string;
+}
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    platform: "twitter",
+    url: "https://twitter.com/ojoanalogo/",
+    label: "X",
+  },
+  {
+    platform: "github",
+    url: "https://github.com/ojoanalogo/",
+    label: "Github",
+  },
+  {
+    platform: "instagram",
+    url: "https://instagram.com/ojo.analogo/",
+    label: "Instagram",
+  },
+  {
+    platform: "linkedin",
+    url: "https://linkedin.com/in/ojoanalogo",
+    label: "Linkedin",
+  },
+];
